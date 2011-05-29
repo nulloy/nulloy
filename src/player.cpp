b5659c910c760b8cbe1e3cc82ad037784c80abdd
Sergey Vlasov <sergey@vlasov.me>
2011-05-29 22:31:07 +0300

    don't restore playback if was stopped

--- src/player.cpp
+++ src/player.cpp
@@ -206,7 +206,11 @@ void NPlayer::restorePlaylist()
 	if (!playlistRowValues.isEmpty()) {
 		if (settings()->value("RestorePlayback").toBool()) {
 			m_playlistWidget->activateRow(playlistRowValues.at(0).toInt());
-			m_playbackEngine->setPosition(playlistRowValues.at(1).toFloat());
+			qreal pos = playlistRowValues.at(1).toFloat();
+			if (pos != 0 && pos != 1)
+				m_playbackEngine->setPosition(pos);
+			else
+				m_playbackEngine->stop();
 		} else {
 			m_playlistWidget->setCurrentRow(playlistRowValues.at(0).toInt());
 		}
